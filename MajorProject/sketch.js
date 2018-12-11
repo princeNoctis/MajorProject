7ed2aa1799d9de52e3c0551cf76c2896c69e16a9
PrinceNoctis <ssaad363@gmail.com>
2018-12-11 13:19:05 -0600

    Update sketch.js

--- MajorProject/sketch.js
+++ MajorProject/sketch.js
@@ -5,11 +5,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let menuNum;
+
+
 function setup() {
   createCanvas(800, 600);
+  menuNum = 0;
 }
 
 function draw() {
+  mainMenu();
+}
+
+
+
+function mainMenu(){
   background(0);
   fill("yellow");
   text("Hello",width/2,height/2);
