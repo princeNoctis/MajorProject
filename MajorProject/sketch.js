daf064559c8b3af40c3dca993214b96a4e94923f
PrinceNoctis <ssaad363@gmail.com>
2018-12-13 14:05:35 -0600

    images for menu

--- MajorProject/sketch.js
+++ MajorProject/sketch.js
@@ -6,13 +6,16 @@
 // - describe what you did to take this project "above and beyond"
 
 let menuNum;
-let levelBackground;
+let levelBackground,lives,randomImg;
 
 
 function setup() {
   createCanvas(800, 600);
   levelBackground = loadImage("assets/lvlbckgrnd.png");
+  lives = loadImage("assets/Heart.png");
+  randomImg = loadImage("assets/marioGif.gif");
   menuNum = 0;
+  Animation(label,animation);
 }
 
 function draw() {
@@ -25,19 +28,25 @@ function mainMenu(){
   if (menuNum === 0){
     background(0);
     fill("yellow");
-    text("Main Menu",width/2,height/2);
-    textSize(60);
+    textSize(25);
     textAlign(CENTER, CENTER);
-
     text("Play",width/2,400);
+    image(randomImg,200,300,200,200);
     textAlign(CENTER);
   }
-  else{
-    loadBackground();
+  else if (menuNum === 1){
+    drawLives();
   }
 
 }
 
 function loadBackground(){
   background(levelBackground);
+}
+
+function drawLives(){
+  imageMode(CORNER);
+  image(lives,50,50,25,25);
+  image(lives,75,50,25,25);
+  image(lives,100,50,25,25);
 }
