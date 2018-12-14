196005753b6b43d9f4182bf0101b126f756baf84
PrinceNoctis <ssaad363@gmail.com>
2018-12-14 14:13:23 -0600

    mario imge

--- MajorProject/sketch.js
+++ MajorProject/sketch.js
@@ -6,52 +6,100 @@
 // - describe what you did to take this project "above and beyond"
 
 let menuNum;
-let playButtonX = 200 ,playButtonY = 200;
+let playButtonX = 400 ,playButtonY = 400;
 let levelBackground,lives,randomImg;
+let menuMusic,gameMusic;
+let tiles,levelToLoad,lines,tilesWidth,tilesHeight;
+let character,coins,platform,koomba;
 
 
 function preload(){
   randomImg = loadAnimation("assets/frame_0_delay-0.05s.png","assets/frame_1_delay-0.05s.png");
+  levelToLoad = "assets/Lvl.1.txt";
+  lines = loadStrings(levelToLoad);
+  platform = loadImage("assets/platform.png");
+  // coins = loadImage("images/coin.png");
+  // koomba = loadImage("images/koomba.png");
 }
 
 
 function setup() {
   createCanvas(800, 600);
-  levelBackground = loadImage("assets/lvlbckgrnd.png");
   lives = loadImage("assets/Heart.png");
   menuNum = 0;
+  tilesHeight = lines.length;
+  tilesWidth = lines[0].length;
+  tiles = createEmpty2dArray(tilesWidth, tilesHeight);
+  for (let y = 0; y < tilesHeight; y++) {
+    for (let x = 0; x < tilesWidth; x++) {
+      let tileType = lines[y][x];
+      tiles[x][y] = tileType;
+    }
+  }
 }
 
 function draw() {
   mainMenu();
 }
 
+function display() {
+  for (let y = 0; y < tilesHeight; y++) {
+    for (let x = 0; x < tilesWidth; x++) {
+      showTile(tiles[x][y], x, y);
+    }
+  }
+}
+
 
 
 function mainMenu(){
   if (menuNum === 0){
-    background("white");
-    fill("black");
-    textSize(25);
-    textAlign(CENTER, CENTER);
-    rectMode(CENTER);
-    animation(randomImg,200,300);
-    text("Play",width/2,400);
-    textAlign(CENTER);
-    drawLives();
 
-    stroke("black");
-    fill("green");
-    rect(playButtonX,playButtonY);
+    strokeWeight(10);
+    rect(0, 0, width, height);
+    let hitbox = collidePointRect(mouseX, mouseY, playButtonX, playButtonY, 150, 50);
+    if (hitbox) {
+      fill("lightgreen");
+    }
+    else {
+      fill("green");
+    }
+    push();
+    rect(playButtonX, playButtonY, 150, 50);
+    textSize(25);
+    fill("black");
+    text("play",455,435);
+    //text on the home screen...
+    textSize(15);
+    text("A Major Computer Programing Project by Muhammad .S", 50, 550);
+    textSize(40);
+    text("Platformer!", 130, 150);
+    pop();
+    animation(randomImg,300,400);
   }
   else if (menuNum === 1){
-    loadBackground();
+    display();
   }
-
 }
 
+function showTile(location, x, y) {
+  if (location === "0") {
+    image(platform, x * tilesWidth, y * tilesHeight, tilesWidth, tilesHeight);
+  }
+}
+
+
 function loadBackground(){
-  background(levelBackground);
+  for(let x = 0; 0 < tilesWidth;x++){
+    for(let y = 0; 0 < tilesHeight; y++){
+      if(tiles[x][y] === "0"){
+        fill("blue");
+      }
+      if (tiles[x][y]=== "1"){
+        fill("purple");
+      }
+    }
+  }
 }
 
 function drawLives(){
@@ -59,4 +107,25 @@ function drawLives(){
   image(lives,50,50,25,25);
   image(lives,75,50,25,25);
   image(lives,100,50,25,25);
+}
+function player(){
+
+}
+
+function createEmpty2dArray(cols, rows) {
+  let randomGrid = [];
+  for (let x = 0; x < cols; x++) {
+    randomGrid.push([]);
+    for (let y = 0; y < rows; y++) {
+      randomGrid[x].push(0);
+    }
+  }
+  return randomGrid;
+}
+
+function mousePressed() {
+  let startGame = collidePointRect(mouseX, mouseY, playButtonX, playButtonY, 150, 50);
+  if (startGame === true && menuNum === 0){
+    menuNum = 1;
+  }
 }
