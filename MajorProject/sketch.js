f3e5a73b4c2043d60e98ea47024c4fcc4aac4ee4
PrinceNoctis <ssaad363@gmail.com>
2018-12-14 13:03:48 -0600

    added a animation

--- MajorProject/sketch.js
+++ MajorProject/sketch.js
@@ -6,16 +6,20 @@
 // - describe what you did to take this project "above and beyond"
 
 let menuNum;
+let playButtonX = 200 ,playButtonY = 200;
 let levelBackground,lives,randomImg;
+
+
+function preload(){
+  randomImg = loadAnimation("assets/frame_0_delay-0.05s.png","assets/frame_1_delay-0.05s.png");
+}
 
 
 function setup() {
   createCanvas(800, 600);
   levelBackground = loadImage("assets/lvlbckgrnd.png");
   lives = loadImage("assets/Heart.png");
-  randomImg = loadImage("assets/marioGif.gif");
   menuNum = 0;
-  Animation(label,animation);
 }
 
 function draw() {
@@ -26,16 +30,22 @@ function draw() {
 
 function mainMenu(){
   if (menuNum === 0){
-    background(0);
-    fill("yellow");
+    background("white");
+    fill("black");
     textSize(25);
     textAlign(CENTER, CENTER);
+    rectMode(CENTER);
+    animation(randomImg,200,300);
     text("Play",width/2,400);
-    image(randomImg,200,300,200,200);
     textAlign(CENTER);
+    drawLives();
+
+    stroke("black");
+    fill("green");
+    rect(playButtonX,playButtonY);
   }
   else if (menuNum === 1){
-    drawLives();
+    loadBackground();
   }
 
 }
