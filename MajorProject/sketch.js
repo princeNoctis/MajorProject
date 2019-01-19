4035fbc36722feda8b1c2408458ad7d2aa6f5329
princeNoctis <43047382+princeNoctis@users.noreply.github.com>
2019-01-19 14:44:15 -0600

    FINISHED

--- MajorProject/sketch.js
+++ MajorProject/sketch.js
@@ -7,6 +7,8 @@
 // Resources - https://answers.unity.com/questions/239614/roll-a-ball-towards-the-player.html for the
 // ball being spit towards the player ai against
 
+
+// all the variables and states and coordinates
 let backgroundMusic;
 let victorySound,gameOverSound;
 let menuScreen, menuScreenSound;
@@ -81,12 +83,13 @@ let difficulty;
 let ganondorfLeft;
 let ganondorfRight;
 
+
 //state -1 = victoryscreen
 //state -2 = game OVER
 //state -3 = ???
 //state 1 = game
 
-
+// preload function loading images and sounds
 function preload(){
   bg1 = loadImage("assets/PNG/Hills Layer 01.png");
   bg2 = loadImage("assets/PNG/Hills Layer 02.png");
@@ -117,25 +120,26 @@ function preload(){
   ganondorfRight = loadImage("assets/ganondorfright.png");
   menuScreen = loadAnimation("assets/backgorun/frame_0_delay-0.1s.png","assets/backgorun/frame_1_delay-0.1s.png","assets/backgorun/frame_2_delay-0.1s.png","assets/backgorun/frame_3_delay-0.1s.png","assets/backgorun/frame_4_delay-0.1s.png","assets/backgorun/frame_5_delay-0.1s.png");
 }
-
+// setup function
 function setup() {
   createCanvas(1320, 640);
+  lose = false;
   bx = width;
   by = 50;
-  menuScreenSound.play();
+  menuScreenSound.loop();
   menuScreenSound.setVolume(0.8);
   bird = createSprite(bx,by, 24, 24);
   bird.addAnimation("assets/bird/frame_0_delay-0.1s.png","assets/bird/frame_1_delay-0.1s.png","assets/bird/frame_2_delay-0.1s.png","assets/bird/frame_3_delay-0.1s.png","assets/bird/frame_4_delay-0.1s.png","assets/bird/frame_5_delay-0.1s.png");
   linkX = 150;
   linkY = 300;
   linkFacing = "right";
-
+// physics stuff
   velocityY = 0;
   gravity = 1;
   velocityX = 0;
   sprintTimer = 0;
   sprintCooldown = 0;
-
+// ATTACK COOLDOWN AND STUFF
   smashTimer = 0;
   smashCooldown = 0;
   smash = 0;
@@ -143,7 +147,7 @@ function setup() {
   state = -3;
   tutorialstate = 0;
   tutorialTimer = 0;
-
+// ENEMY VAULES
   enemystate = 0;
   enemyTimer = 0;
   enemyX = 700;
@@ -166,7 +170,7 @@ function setup() {
 
   difficulty = 0;
 }
-
+// DRAW FUNCTION
 function draw() {
   parallaxEffect();
   player();
@@ -179,7 +183,9 @@ function draw() {
   }
   if (state === -2) {
     drawGameOver();
-    menuScreenSound.stop();
+    // gameOverSound.setVolume(0.2);
+    // gameOverSound.play();
+    // menuScreenSound.stop();
   }
   if (state === -1) {
     drawVictory();
@@ -202,20 +208,17 @@ function draw() {
 function parallaxEffect(){
   background(bg1);
 
-  image(bg1,-0.05*linkX,5,1320,640);
-  image(bg1,-0.05*linkX+1320,5,1320,640);
+  image(bg2,0 -1*linkX%1320, 5, 1320, 640);// the first image is before the parallax effect happens
+  image(bg2,0 -1*linkX%1320+ 1320,5, 1320, 640); // The second picture is used to have the illusion of an infinite background
 
-  image(bg2, -0.5*linkX, 5, 1320, 640);// the first image is before the parallax effect happens
-  image(bg2, -0.5*linkX+ 1320,5, 1320, 640); // The second picture is used to have the illusion of an infinite background
+  image(bg3,0- 1.25*linkX%1320,0, 1320, 640);
+  image(bg3,0- 1.25*linkX%1320+1320, 0, 1320, 640);
 
-  image(bg3,- 1*linkX,0, 1320, 640);
-  image(bg3,- 1*linkX+1320, 0, 1320, 640);
+  image(bg4,0- 1.50*linkX%1320, 0, 1320, 640);
+  image(bg4,0- 1.50*linkX%1320+1320, 0, 1320, 640);
 
-  image(bg4,- 1.25*linkX, 0, 1320, 640);
-  image(bg4,- 1.25*linkX+1320, 0, 1320, 640);
-
-  image(bg5,- 1.50*linkX, 0, 1320, 640);
-  image(bg5,- 1.50*linkX+1320, 0, 1320, 640);
+  image(bg5,0- 1.75*linkX%1320, 0, 1320, 640);
+  image(bg5,0- 1.75*linkX%1320+1320, 0, 1320, 640);
 
 }
 /////////// the tutorial //////////////////////////////////
@@ -274,9 +277,10 @@ function drawTutorial(){
   }
   else if (tutorialstate === 10) {
     state = 1;
+    /////////// START GAME/////////////////////////////
   }
 }
-
+//////////////////////////// GAME OVER SCREEN ///////////////////////////////////////////
 function drawGameOver() {
   textSize(25);
   fill("white");
@@ -302,7 +306,7 @@ function drawGameOver() {
     enemyX = 800;
     enemyY = -241;
   }
-
+////////////////// CHANGE GAMEMODE/////////////////////////
   if (keyIsDown(49)) {
     state = 1;
     difficulty = 0;
@@ -312,7 +316,7 @@ function drawGameOver() {
     difficulty = 1;
   }
 }
-
+//////////////////// SHOW THE HEARTS//////////////////////
 function drawPlayersHealth() {
   if (linkHP > 0) {
     image(heart, 35, 20, 40, 40);
@@ -324,7 +328,7 @@ function drawPlayersHealth() {
     image(heart, 105, 20, 40, 40);
   }
 }
-
+////////////////// SHOW ENEMY HEARTS /////////////////////////////////
 function drawEnemyHealth(){
   if (enemyHP > 0) {
     image(enemyHealths, 1220, 20, 40, 40);
@@ -386,206 +390,173 @@ function drawEnemyHealth(){
     image(enemyHealths, 680, 20, 40, 40);
   }
 }
-//
-// let end = false;
-// function endPoint(x, y) {
-//   push();
-//   fill(20);
-//   rect(x, y - 100, 50, 100);
-//   pop();
-//
-//   if (x <= 1320/2) {
-//     end = true;
-//   }
-//
-//   if (collideRectRect(x, y - 100, 50, 100, linkX, linkY, 120, 160)) {
-//     outsideOfLevel = false;
-//     insideOfLevel = true;
-//   }
-// }1
-
-// function playerLink(){
-//   link = createSprite(linkX,linkY,120,160);
-//   link.addAnimation("facing right","assets/a/linkRight.png","assets/a/linkRight2.png","assets/a/linkRight3.png","assets/a/linkRight4.png","assets/a/linkRight5.png","assets/a/linkRight6.png");
-//   link.addAnimation("facing left","assets/linkright/linkleft.png","assets/linkright/linkRight2.png","assets/linkright/linkRight3.png","assets/linkright/linkRight4.png","assets/linkright/linkRight5.png","assets/linkright/linkRight6.png");
-//   drawSprite();
-// }
-
+///////////////////////  DRAW SLIME IMAGES AND SLIME AI /////////////////////////////////////////////
 function drawSlime() {
-
   if (enemyHP < 1) {
     state = -1;
   }
-
-  if (smash === 1) {
-    if (linkFacing === "right") {
-
-      if ( abs( linkX - enemyX - 40 ) < 200 && abs( linkY - enemyY - 40 ) < 200 ) {
-        enemyHit = 1;
-      }
-    }
+/////////////// IF PLAYER HITS ENEMY ///////////////////////////////////////////
+	if (smash === 1) {
+		if (linkFacing === "right") {
+			if ( abs( linkX - enemyX - 40 ) < 200 && abs( linkY - enemyY - 40 ) < 200 ) {
+				enemyHit = 1;
+			}
+		}
     else {
-      if ( abs( linkX - enemyX - 200 ) < 200 && abs( linkY - enemyY - 40 ) < 200 ) {
-        enemyHit = 1;
-      }
-    }
-  }
-
+			if ( abs( linkX - enemyX - 200 ) < 200 && abs( linkY - enemyY - 40 ) < 200 ) {
+					enemyHit = 1;
+			}
+		}
+	}
+/////////////////// SHOW THE RED IMAGE OF SLIME //////////////////////////
   if (enemyHit === 1) {
-    if (enemygoRed === 0) {
-      enemyHP--;
-      enemygoRed = 20;
-    }
+		if (enemygoRed === 0) {
+			enemyHP--;
+			enemygoRed = 20;
+		}
     else if (enemygoRed > 1) {
-      enemygoRed--;
-    }
+			enemygoRed--;
+		}
     else {
       enemyHit = 0;
       enemygoRed--;
     }
   }
-
-  if (enemyHit === 0 || enemygoRed % 10 < 5) {
-    if (enemyState === "slime") {
-      image(slimeBoss, enemyX, enemyY, 320, 250);
-    }
-    if (enemyState === "Predict") {
-      image(slimeBossPredict, enemyX, enemyY, 320, 250);
-    }
-    if (enemyState === "spit") {
-      image(slimeBossSpit, enemyX, enemyY, 320, 250);
-    }
-    if (enemyState === "charge") {
-      image(slimeBossCharge, enemyX, enemyY, 320, 250);
-    }
-    if (enemyState === "charge") {
-      image(slimeBossCharge, enemyX, enemyY, 320, 250);
-    }
-  }
-  else {
-    image(slimeBossRed, enemyX, enemyY, 320, 250);
-  }
-
-  if ( abs(linkX - enemyX - 120 ) < 200 && abs( linkY - enemyY - 40 ) < 200 ) {
-    linkHit = 1;
-  }
-  ////////////////////////////////////////////////////////
-  if (enemygoRed === 0) {
-    if (enemyTimer < 1) {
+  ///////////// CALLING THE DIFFERENT PHASES OF THE SLIME //////////////////////////////
+	if (enemyHit === 0 || enemygoRed % 10 < 5) {
+		if (enemyState === "slime") {
+			image(slimeBoss, enemyX, enemyY, 320, 240);
+		}
+		if (enemyState === "tell") {
+			image(slimeBossPredict, enemyX, enemyY, 320, 240);
+		}
+		if (enemyState === "spit") {
+			image(slimeBossSpit, enemyX, enemyY, 320, 240);
+		}
+		if (enemyState === "charge") {
+			image(slimeBossCharge, enemyX, enemyY, 320, 240);
+		}
+	} else {
+		image(slimeBossRed, enemyX, enemyY, 320, 240);
+	}
+//////////////////////////// IF ENEMY HITS PLAYER //////////////////////////////
+	if ( abs( linkX - enemyX - 120 ) < 200 && abs( linkY - enemyY - 40) < 200 ) {
+		linkHit = 1;
+	}
+///////////////////////// PHASE ONE FOR SLIME ////////////////////////////////////////////////////////
+ 	if (enemystate === 0) {
+		if (enemyTimer < 1) {
       linkX = 160;
-      linkY = 400;
+		  linkY = 400;
       enemyTimer++;
-    }
+		}
     else if (enemyTimer < 34) {
-      enemyVelocityY++;
-      enemyY = enemyY + enemyVelocityY;
-      enemyTimer++;
-    }
+			enemyVelocityY++;
+			enemyY = enemyY + enemyVelocityY;
+			enemyTimer++;
+		}
     else if (enemyTimer < 64) {
-      image(slimeBossSpikes, enemyX - 160, enemyY + 80, 160, 160);
-      image(slimeBossSpikes, enemyX + 320, enemyY + 80, 160, 160);
-
-      if ( abs(linkX - enemyX - 120) < 360 && abs(linkY - enemyY - 120) < 160) {
-        linkHit = 1;
-      }
-      enemyTimer++;
-    }
+			image(slimeBossSpikes, enemyX - 160, enemyY + 80, 160, 160);
+			image(slimeBossSpikes, enemyX + 320, enemyY + 80, 160, 160);
+			if ( abs(linkX - enemyX - 120) < 360 && abs(linkY - enemyY - 120) < 160) {
+				linkHit = 1;
+			}
+			enemyTimer++;
+		}
+    else {
+	  	enemyTimer = 0;
+			enemystate = 1;
+		}
+	}
+  //////////////////// SECOND PHASE ////////////////////////////////////////
+	if (enemystate === 1) {
+		if (enemyTimer < 50 && difficulty === 0) {
+			enemyTimer++;
+			enemyState = "slime";
+		}
     else {
       enemyTimer = 0;
-      enemygoRed = 1;
-    }
-  }
-  //////////////////////////////////////////////////////////////
-  if (enemygoRed === 1) {
-    if (enemyTimer < 50 && difficulty === 0) {
-      enemyTimer++;
-      enemyState = "slime";
-    }
-    else {
-      enemyTimer = 0;
-      enemygoRed = 2 + floor( random() * 3 );
-    }
-  }
-  //////////////////////////////////////////////
-  if (enemygoRed === 2) {
-    if (enemyTimer < 50) {
-      enemyState = "charge";
-      enemyTimer++;
-    }
+			enemystate = 2 + floor( random() * 3 );
+		}
+	}
+  ////////////////// THRID PHASE //////////////////////////////////////
+	if (enemystate === 2) {
+		if (enemyTimer < 50) {
+			enemyState = "charge";
+			enemyTimer++;
+		}
     else if (enemyTimer < 100 - 25 * difficulty ) {
-      enemyTimer++;
-      if (enemySide === "right") {
-        enemyX = enemyX - 16 + 16 * difficulty;
-      }
+			enemyTimer++;
+			if (enemySide === "right") {
+				enemyX = enemyX - (16 + 16 * difficulty);
+			}
       else {
-        enemyX = enemyX + 16 + 16 * difficulty;
-      }
-    }
-    else if (enemyTimer < 115 - 25 * difficulty ) {
-      enemyTimer++;
-    }
+				enemyX = enemyX + (16 + 16 * difficulty);
+			}
+		}
+    else if (enemyTimer < 115 - 25 * difficulty) {
+			enemyTimer++;
+		}
     else if (enemyTimer < 135 - 25 * difficulty ) {
-      enemyTimer++;
-      enemyState = "slime";
-      if (enemySide === "right") {
-        enemyX = enemyX + 8;
-      }
+			enemyTimer++;
+			enemyState = "slime";
+			if (enemySide === "right") {
+				enemyX = enemyX + 8;
+			}
       else {
-        enemyX = enemyX - 8;
-      }
-    }
+				enemyX = enemyX - 8;
+			}
+		}
     else if (enemyTimer < 155 - 25 * difficulty ) {
-      enemyTimer++;
-    }
+			enemyTimer++;
+		}
     else {
-      if (enemySide === "right") {
-        enemySide = "left";
-      }
+			if (enemySide === "right") {
+				enemySide = "left";
+			}
       else {
-        enemySide = "right";
-      }
-      enemyTimer = 0;
-      enemygoRed = 1;
-    }
-  }
-  //////////////////////////////////////////////
-  if (enemygoRed === 3) {
-    if (enemyTimer < 1) {
-      enemyState = "slime";
-      enemyVelocityY = -34;
-      enemyTimer++;
-    }
+				enemySide = "right";
+			}
+			enemyTimer = 0;
+			enemystate = 1;
+		}
+	}
+ ///////////// FOURTH PHASE /////////////////////////////////////
+	if (enemystate === 3) {
+		if (enemyTimer < 1) {
+			enemyState = "slime";
+			enemyVelocityY = -34;
+			enemyTimer++;
+		}
     else if (enemyTimer < 68) {
-      enemyVelocityY++;
-      enemyY = enemyY + enemyVelocityY;
-      enemyTimer++;
-    }
+			enemyVelocityY++;
+			enemyY = enemyY + enemyVelocityY;
+			enemyTimer++;
+		}
     else if (enemyTimer < 98) {
-      image(slimeBossSpikes, enemyX - 160, enemyY + 80, 160, 160);
-      image(slimeBossSpikes, enemyX + 320, enemyY + 80, 160, 160);
-      enemyTimer++;
-      if ( abs(linkX - enemyX - 120) < 360 && abs(linkY - enemyY - 120) < 160) {
-        linkHit = 1;
-      }
-    }
+			image(slimeBossSpikes, enemyX - 160, enemyY + 80, 160, 160);
+			image(slimeBossSpikes, enemyX + 320, enemyY + 80, 160, 160);
+			enemyTimer++;
+			if ( abs(linkX - enemyX - 120) < 360 && abs(linkY - enemyY - 120) < 160) {
+				linkHit = 1;
+			}
+		}
     else {
-      enemyTimer = 0;
-      enemygoRed = 1;
-    }
-  }
-  ////////////////////////////////////////////////
-  if (enemygoRed === 4) {
-
-    if (enemyTimer < 49) {
-      enemyState = "Predict";
-      enemyTimer++;
-    }
+	  	enemyTimer = 0;
+			enemystate = 1;
+		}
+	}
+	if (enemystate === 4) {
+		if (enemyTimer < 49) {
+			enemyState = "tell";
+			enemyTimer++;
+		}
     else if (enemyTimer < 50) {
-
+  ///////////////////////// WHEN THE SLIME SPITS THE BALL GO TOWARDS WERE EVER THE PLAYER IS /////////////////////////////////////////////
       enemyBallX = enemyX + 80;
       enemyBallY = enemyY + 40;
-      enemyBallVelocityX = (linkX - enemyX - 120)/ ( sqrt( pow( linkX - enemyX - 120 , 2 ) + pow( linkY - enemyY - 40 , 2) ) / (12 + 12 * difficulty));
-      enemyBallVelocityY = (linkY - enemyY - 40)/ ( sqrt( pow( linkX - enemyX - 120 , 2) + pow( linkY - enemyY - 40, 2) ) / (12 + 12 * difficulty) );
+      enemyBallVelocityX = (linkX - enemyX - 120) / ( sqrt( pow( linkX - enemyX - 120 , 2 ) + pow( linkY - enemyY - 40 , 2) ) / (12 + 12 * difficulty) );
+      enemyBallVelocityY = (linkY - enemyY - 40) / ( sqrt( pow( linkX - enemyX - 120 , 2) + pow( linkY - enemyY - 40, 2) ) / (12 + 12 * difficulty) );
       enemyTimer++;
     }
     else if (enemyTimer < 150 - 50 * difficulty ) {
@@ -594,19 +565,17 @@ function drawSlime() {
       enemyBallY = enemyBallY + enemyBallVelocityY;
       image(slimeBossBall, enemyBallX, enemyBallY, 160, 160);
       enemyTimer++;
-      if ( abs( linkX - enemyBallX - 40 ) < 120 && abs( linkY - enemyBallY ) < 160 ) {
+      if ( abs( linkX - enemyBallX - 40 ) < 120 && abs( linkY - enemyBallY) < 160 ) {
         linkHit = 1;
       }
-
-
     }
     else {
       enemyTimer = 0;
-      enemygoRed = 1;
+      enemystate = 1;
     }
   }
 }
-
+///////////////////////// WIN SCREEN ///////////////////////////////////////
 function drawVictory() {
   image(victory, 0, 0, 1320,640);
   textSize(25);
@@ -618,14 +587,17 @@ function drawVictory() {
     linkHit = 0;
     linkHitRed = 0;
     linkHP = 3;
+
     enemyHit = 0;
     enemystate = 0;
     enemyHP = 10;
+
     smash = 0;
     sprintTimer = 0;
     sprintCooldown = 0;
     smashTimer = 0;
     smashCooldown = 0;
+
     enemyState = "slime";
     enemyVelocityY = 0;
     enemySide = "right";
@@ -646,11 +618,13 @@ function drawVictory() {
     difficulty = 1;
   }
 }
-
+///////////////////// ALL THE PLAYER VALUES /////////////////////////
 function player(){
   if (linkHP < 1) {
     state = -2;
-    lose = true;
+  }
+  else {
+    gameOverSound.stop();
   }
 
   if (linkHit === 1) {
@@ -762,7 +736,7 @@ function player(){
     image(linkRed, linkX, linkY, 120, 160);
   }
 }
-
+//////////////////// THE SECOND BOSS //////////////////////////////////////
 function drawSecondBoss() {
 //////////////////////////////////////////////////
 ///////////////////////////////////////////////////
@@ -781,7 +755,7 @@ function drawSecondBoss() {
 ////
 //
 }
-
+// THE RANDOM BIRD ON THE TOP RIGHT CORNER OF SCREEN DOESNT WORK!!! //
 function randomBird() {
   // Moving up at a constant speed
   bird.position.bx = bird.position.bx - 0.5;
@@ -791,8 +765,7 @@ function randomBird() {
     bird.position.bx = width;
   }
 }
-
-
+////////////////////// AND THE MAIN MENU /////////////////////////////////////////////
 function Menu(){
   frameRate(60);
   animation(menuScreen, 660, 320);
---
+++ beta testing.md
@@ -0,0 +1,10 @@
+BETA TEST
+====================
+THE BETA TESTERS
+
+### TEST #1 BY SANTANU DEB ###
+ - 
+
+
+### TEST #1 BY SHADMAN ###
+ - 
